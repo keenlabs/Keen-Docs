02cd681a0f9b15b276fb14130dc8f7fbdeb85ac7
Ryan Spraetz <spraetz@gmail.com>
2012-08-21 18:52:01 -0700

    Make awesome modz

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 cd "$(dirname "$0")"
 
 : ${KEEN_WEB_DIR?"Need to set KEEN_WEB_DIR environment variable"}
-KEEN_NEW_DOCS_FOLDER="$KEEN_WEB_DIR/app/static/new-docs"
+KEEN_NEW_DOCS_FOLDER="$KEEN_WEB_DIR/app/static/docs"
 
 echo "Okay, first I'm going to run Sphinx."
 
 make clean html
 
-echo "Okay, that worked, now to delete everything in the current Keen-Web new-docs folder: $KEEN_NEW_DOCS_FOLDER"
+echo "Okay, that worked, now to delete everything in the current Keen-Web docs folder: $KEEN_NEW_DOCS_FOLDER"
 
 rm -rf $KEEN_NEW_DOCS_FOLDER
 
